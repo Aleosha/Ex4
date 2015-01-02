8395b221b7d12f4ee06e598b584c648a6a16ec56
Aleosha <aleosha@yandex.ru>
2015-01-02 19:31:08 +0200

    Add classes stubs

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -10,6 +10,29 @@ namespace Test
     {
         static void Main(string[] args)
         {
+            displayInterfacesMenu();
+            displayDelegatesMenu();
+        }
+
+        private static void displayDelegatesMenu()
+        {
+            //Create main menu
+            //Add Words counter to main menu
+            //Add date sub menu to main menu
+            //Add Show date to sub menu
+            //Add Show time to sub menu
+            //Add Show version to main menu
+        }
+
+        private static void displayInterfacesMenu()
+        {
+            //Create main menu
+            Interfaces.MainMenu mainMenu = new Interfaces.MainMenu();
+            //Add Words counter to main menu
+            //Add date sub menu to main menu
+            //Add Show date to sub menu
+            //Add Show time to sub menu
+            //Add Show version to main menu
         }
     }
 }
